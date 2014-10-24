eeb1c3fcee65062b90d9d1a3b6e6b6da1c278791
Matthew <mrk45@uclive.ac.nz>
2014-10-24 22:15:18 +1300

    Initial Commit

---
+++ ExamplePlugin/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using PluginCommon;
+using PluginInterface;
+
+namespace ExamplePlugin
+{
+    public class Plugin : IPlugin
+    {
+        public string Name { get { return "Test"; } }
+        public string Description { get { return "Desc"; } }
+        public string Author { get { return "Matthew"; } }
+        public Version Version { get{return new Version();} }
+        public PluginOption[] ProvidesOptions { get { return null; } }
+        public void Initialise(string[] args)
+        {
+            dynamic a = new Class1();
+            PluginLoader.Register(Assembly.GetEntryAssembly(), "PluginApp.Program", new[] { a });
+        }
+
+        public void Dispose()
+        {
+            return;
+        }
+    }
+
+    public class Class1 : IConsole
+    {
+        public void Run()
+        {
+            Console.WriteLine("Test1234");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PluginLoader/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using PluginInterface;
+
+namespace PluginApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pluginDir = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Plugins");
+            string pluginDir = Path.GetFullPath(@"..\..\..\..\PluginLoader\ExamplePlugin\bin\Debug");
+
+            var loader = new PluginCommon.PluginLoader(pluginDir);
+            loader.PluginLoadComplete += PluginLoadComplete;
+            loader.PluginLoadFailed += PluginLoadFailed;
+            loader.PluginLoadingComplete += PluginLoadingComplete;
+            loader.PluginLoadingStarted += PluginLoadingStarted;
+            loader.GetPlugins();
+            loader.InitialisePlugins(args);
+            Console.ReadKey();
+        }
+
+        private static void PluginLoadingStarted(PluginCommon.PluginLoader sender, object otherData)
+        {
+            Console.WriteLine("Loading Started...");
+        }
+
+        private static void PluginLoadingComplete(PluginCommon.PluginLoader sender, object otherData)
+        {
+            Console.WriteLine("Loading Complete");
+        }
+
+        private static void PluginLoadFailed(PluginCommon.PluginLoader sender, object otherData)
+        {
+            Console.WriteLine("Load of plugin failed");
+        }
+
+        private static void PluginLoadComplete(PluginCommon.PluginLoader sender, object otherData)
+        {
+            Console.WriteLine("Load of plugin complete");
+        }
+
+        public static bool RegisterPlugin(dynamic plugin)
+        {
+            Console.WriteLine("Registering plugin...");
+            if (plugin is IConsole)
+            {
+                var p = (IConsole) plugin;
+                p.Run();
+                return true;
+            }
+            Console.WriteLine("Failed");
+            return false;
+        }
+    }
+}
